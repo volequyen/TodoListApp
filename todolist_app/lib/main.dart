97e50bf6db093544c1525d0e3c0b10fadf563e13
volequyen <volequyen11042005@gmail.com>
2025-04-01 00:29:53 +0700

    firstUI

---
+++ todolist_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+      title: Center(
+        child: Text(
+          'ToDoList',
+          style: TextStyle(fontSize: 25)
+          ),
+      ),
+      backgroundColor: Colors.pinkAccent,
+     ),
+     body: Container(),
+     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Icon(Icons.add, size: 40, color: Colors.white,),
+      backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
