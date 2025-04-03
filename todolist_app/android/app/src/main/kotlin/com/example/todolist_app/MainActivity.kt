13750f33642d9eaa63a1a3dbabac72df1460f423
volequyen <volequyen11042005@gmail.com>
2025-04-03 23:45:34 +0700

    Done

--- todolist_app/android/app/src/main/kotlin/com/example/todolist_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.todolist_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- todolist_app/lib/Modal/item.dart
+++
@@ -1,7 +0,0 @@
-class DataItem{
-  final String id;
-  final String name;
-  DataItem({
-    required this.id, 
-    required this.name});
-}
